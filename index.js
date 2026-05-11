73ff713f90728ac7a0629348abfa47b145e72544
Sintya Khofifah <sintyakhofifah4108@gmail.com>
2026-05-11 14:41:01 +0700

    add create-va endpoint

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // ── Firebase Admin ────────────────────────────────────────────────────────────
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
+
 const db = admin.firestore();
 
 // ── Midtrans clients ──────────────────────────────────────────────────────────
