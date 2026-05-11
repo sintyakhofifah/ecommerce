f542b4c26c4d230a51ee17eb1a2d0d47ec55c016
Sintya Khofifah <sintyakhofifah4108@gmail.com>
2026-05-11 12:44:18 +0700

    update

--- index.js
+++ index.js
@@ -333,7 +333,7 @@ app.post('/notification', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
