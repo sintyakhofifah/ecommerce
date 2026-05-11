b5b688bf04b9a02cf8210614ebdfc55e7aa97dad
Sintya Khofifah <sintyakhofifah4108@gmail.com>
2026-05-11 14:26:21 +0700

    add create-va endpoint

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // ── Firebase Admin ────────────────────────────────────────────────────────────
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  }),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert({
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//   }),
+// });
 
 const db = admin.firestore();
 
